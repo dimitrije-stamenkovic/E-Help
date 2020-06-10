c08397140599e9dceda07a5e078119c7206192c8
stefi51 <stefi.morningstar@elfak.rs>
2020-06-10 10:55:15 +0200

    Add signin,signup

---
+++ app/src/main/java/dimitrijestefan/mosis/ehelp/Models/User.kt
@@ -0,0 +1,11 @@
+package dimitrijestefan.mosis.ehelp.Models
+
+data class User (
+    var username:String,
+    var password:String,
+    var email:String,
+    var name:String,
+    var lastname:String,
+    var number:String
+)
+
