df4ef45821b989a09ff692f0012ec705245c80e8
stefi51 <stefi.morningstar@elfak.rs>
2020-06-17 21:34:14 +0200

    RecyclerViewFriendRequest

--- app/src/main/java/dimitrijestefan/mosis/ehelp/AddFriendFragment.kt
+++ app/src/main/java/dimitrijestefan/mosis/ehelp/AddFriendFragment.kt
@@ -6,11 +6,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import dimitrijestefan.mosis.ehelp.Adapters.RecycleViewAdapterAddFriends
+import dimitrijestefan.mosis.ehelp.Models.User
+import kotlinx.android.synthetic.main.fragment_add_friend.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class AddFriendFragment : Fragment() {
+class AddFriendFragment : Fragment(),RecycleViewAdapterAddFriends.OnItemClickListener,RecycleViewAdapterAddFriends.OnImageClickListener {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,5 +26,22 @@ class AddFriendFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_add_friend, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recycleViewAddFriend.layoutManager= LinearLayoutManager(requireContext(),LinearLayout.VERTICAL, false)
+        var users= ArrayList<User>()
+        users.add(User("Maname","Maname","Maname","Maname","Maname","Maname","Maname"))
+        users.add(User("Maname1","Maname","Maname","Maname","Maname","Maname","Maname"))
+    var adapter= RecycleViewAdapterAddFriends(users, this, this)
+        recycleViewAddFriend.setAdapter(adapter)
+    }
+
+    override fun onItemClick(position: Int) {
+        Toast.makeText(this.activity, position.toString(), Toast.LENGTH_LONG).show()
+    }
+
+    override fun onImageClick(position: Int) {
+        Toast.makeText(this.activity, "kliknuta slika "+position.toString(), Toast.LENGTH_LONG).show()
+    }
 
 }
