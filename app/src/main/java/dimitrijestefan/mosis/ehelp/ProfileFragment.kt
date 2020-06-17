d3d3fc6bb4909e54026f8bda67aa54a0bd2c4d1f
stefi51 <stefi.morningstar@elfak.rs>
2020-06-17 10:03:01 +0200

    ProfileToolbar

--- app/src/main/java/dimitrijestefan/mosis/ehelp/ProfileFragment.kt
+++ app/src/main/java/dimitrijestefan/mosis/ehelp/ProfileFragment.kt
@@ -3,10 +3,15 @@ package dimitrijestefan.mosis.ehelp
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.*
+import android.view.Gravity.*
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.google.firebase.auth.FirebaseAuth
 import dimitrijestefan.mosis.ehelp.Activity.SignInActivity
 import kotlinx.android.synthetic.main.fragment_profile.*
@@ -15,25 +20,38 @@ import kotlinx.android.synthetic.main.fragment_profile.*
  * A simple [Fragment] subclass.
  */
 class ProfileFragment : Fragment() {
+    private lateinit var mDrawerToggle:ActionBarDrawerToggle
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view:View=inflater.inflate(R.layout.fragment_profile, container, false)
+        val toolbar:Toolbar=view.findViewById<Toolbar>(R.id.rightMenuToolbar)
+        val drawerLayout= view.findViewById<DrawerLayout>(R.id.drawer_layout)
+        toolbar.inflateMenu(R.menu.profile_right_menu)
+        toolbar.setOnMenuItemClickListener {
+            if(it.itemId==R.id.show_settings) {
+                drawerLayout.openDrawer(Gravity.END)
+            }
+             true
+        }
+
+        return  view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        mDrawerToggle=ActionBarDrawerToggle(this.requireActivity(),drawer_layout,rightMenuToolbar,R.string.open, R.string.close)
+        mDrawerToggle.isDrawerIndicatorEnabled = false
+        mDrawerToggle!!.syncState()
         btnSignOut.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
             val intent= Intent(requireContext(), SignInActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(intent)
             requireActivity().finish()
-
-
         }
     }
 
