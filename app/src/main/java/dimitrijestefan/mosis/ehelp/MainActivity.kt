146b27f3524e27f1c1a79829e44f2fdb7fbeca33
mita99796 <muskitos.97@gmail.com>
2020-06-20 14:46:15 +0200

    Location service implemented

--- app/src/main/java/dimitrijestefan/mosis/ehelp/MainActivity.kt
+++ app/src/main/java/dimitrijestefan/mosis/ehelp/MainActivity.kt
@@ -1,17 +1,19 @@
 package dimitrijestefan.mosis.ehelp
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import androidx.navigation.NavController
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.navigation.findNavController
+import dimitrijestefan.mosis.ehelp.Service.LocationService
 
 
 import kotlinx.android.synthetic.main.activity_main.*
+import android.app.ActivityManager
+import android.content.Context
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +25,35 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         NavigationUI.setupWithNavController(bottomnavbar,navController)
 
+        startLocationService()
+
+
+    }
+
+    private fun startLocationService() {
+        if (!isLocationServiceRunning()) {
+            val serviceIntent = Intent(this, LocationService::class.java)
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                this@MainActivity.startForegroundService(serviceIntent) // Da bi ostao da radi, da se ne bi zaustavio
+            } else {
+                startService(serviceIntent)
+            }
+        }
+    }
+
+    private fun isLocationServiceRunning(): Boolean {
+        val manager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        for (service in manager.getRunningServices(Integer.MAX_VALUE)) {
+            if ("com.martinmarinkovic.partytime.LocationService" == service.service.className) {
+                Log.d(
+                    "SERVIS",
+                    "isLocationServiceRunning: location service is already running."
+                )
+                return true
+            }
+        }
+        Log.d("SERVIS", "isLocationServiceRunning: location service is not running.")
+        return false
     }
 
 
---
+++ app/src/main/java/dimitrijestefan/mosis/ehelp/Models/GeoPoint.kt
@@ -0,0 +1,3 @@
+package dimitrijestefan.mosis.ehelp.Models
+
+data class GeoPoint (var latitude:Double,var longitude:Double)
