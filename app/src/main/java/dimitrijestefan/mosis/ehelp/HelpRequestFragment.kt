992c36ea4befc78c74f3e7f3210f361022756fc8
mita99796 <muskitos.97@gmail.com>
2020-07-07 17:06:35 +0200

    small change

--- app/src/main/java/dimitrijestefan/mosis/ehelp/HelpRequestFragment.kt
+++ app/src/main/java/dimitrijestefan/mosis/ehelp/HelpRequestFragment.kt
@@ -6,6 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import dimitrijestefan.mosis.ehelp.Data.AllHelpRequestsData
 import dimitrijestefan.mosis.ehelp.Models.HelpRequest
 import kotlinx.android.synthetic.main.fragment_help_request.*
@@ -17,12 +20,22 @@ private const val ARG_PARAM1 = "helpRequestId"
 
 
 class HelpRequestFragment : Fragment() {
+
+    private val mapViewModel by lazy {
+        ViewModelProvider(requireActivity()).get(MapViewModel::class.java)
+    }
+
     // TODO: Rename and change types of parameters
+
     //private var param1: String? = null
     lateinit var request : HelpRequest
+    lateinit var user_id : String
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        user_id = FirebaseAuth.getInstance().currentUser!!.uid
         arguments?.let {
             //param1 = it.getString(ARG_PARAM1)
             request = it.getString(ARG_PARAM1)?.let { it1 -> AllHelpRequestsData.getRequest(it1) }!!
@@ -43,12 +56,23 @@ class HelpRequestFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         title.text = request.title
         urgency.text = request.urgency
         category.text = request.category
         latitude.text = request.latitude
         longitude.text = request.longitude
         about.text = request.about
+
+        if(AllHelpRequestsData.distance(mapViewModel.current_location.latitude,mapViewModel.current_location.longitude,request.latitude!!.toDouble(),request.longitude!!.toDouble())*1000>10 || request.userId == user_id){
+            help_button.isEnabled = false
+        }
+
+        help_button.setOnClickListener {
+            Toast.makeText(requireContext(),"URADI",Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 
 }
